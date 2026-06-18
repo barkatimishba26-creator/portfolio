bb98206d3ce305caacb422c33e3fcd55a832a47e
barkatimishba26-creator <barkati.mishba26@gmail.com>
2026-06-18 17:01:50 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+   output: "export",
   reactStrictMode: true,
   images: {
     formats: ["image/avif", "image/webp"],
   },
+   basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
 };
 
 export default nextConfig;
